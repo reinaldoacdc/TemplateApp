2d87dede1c8efa40a0b5b285dd3af8ec5cae5200
Reinaldo Alberto <reinaldoacdc@gmail.com>
2023-02-02 20:22:04 +0000

    stringhelper

--- ClassHelpers/StringHelper.pas
+++ ClassHelpers/StringHelper.pas
@@ -5,6 +5,7 @@ interface
 type
   TStringHelper = record helper for string
     function OnlyNumbers :String;
+    function InArray(const ArrayOfString : Array of String) : Boolean;
   end;
 
 implementation
@@ -16,6 +17,20 @@ begin
   for I := 1 To Length(Self) do
     if Self[I] In ['0'..'9'] Then
       Result := Result + Self[I];
+end;
+
+function TStringHelper.InArray(const ArrayOfString : Array of String) : Boolean;
+var
+ Loop : String;
+begin
+  for Loop in ArrayOfString do
+  begin
+    if Self = Loop then
+    begin
+       Exit(true);
+    end;
+  end;
+  result := false;
 end;
 
 end.
